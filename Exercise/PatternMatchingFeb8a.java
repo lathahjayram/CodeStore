2d3142c7be41d650c9ec1e4af770933979591570
Latha Jayram <lathahjayram@gmail.com>
2024-02-08 23:18:09 +0530

    Pattern matching programs

---
+++ Exercise/PatternMatchingFeb8a.java
@@ -0,0 +1,21 @@
+package Exercise;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class PatternMatchingFeb8a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n= sc.nextInt();
+        for(int i =1; i<=n;i++){
+            for(int j=1;j<=n;j++) {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=n;k++){
+                    System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
